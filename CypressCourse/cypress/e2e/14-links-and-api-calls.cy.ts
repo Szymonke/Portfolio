5782ca4406b6955c86d48a7931594b9496e21b4c
Szymonke <dabrowski.szymon1999@gmail.com>
2024-10-16 20:05:18 +0200

    Added solution for Section 9 regarding handling links and intercepting API requests.

---
+++ CypressCourse/cypress/e2e/14-links-and-api-calls.cy.ts
@@ -0,0 +1,30 @@
+describe('Links which open a new tab', () => {
+    beforeEach(() => {
+        cy.visit(`${Cypress.env('demoQA')}/links`)
+    });
+    it('First approach to a link that opens another tab, not clicking the link', () => {
+        cy.get('#simpleLink').then((link)=>{
+            expect(link).to.have.attr('href','https://demoqa.com')
+            expect(link).to.have.attr('target','_blank')
+        })
+    });
+    it('Second approach to a link that opens another tab, removing target attr', () => {
+        cy.get('#simpleLink').invoke('removeAttr','target').click()
+        cy.url().should('eq','https://demoqa.com/')
+    });
+});
+
+describe.only('Intercepting API requests', () => {
+    beforeEach(() => {
+        cy.visit(`${Cypress.env('demoQA')}/links`)
+        cy.intercept('GET', `${Cypress.env('demoQA')}/created`).as('createdStatus')
+    });
+    it('', () => {
+        cy.get('#created').click()
+        cy.wait('@createdStatus').then((interception)=>{
+            cy.log('This is the interception: ', interception)
+            expect(interception.response.statusCode).to.be.equal(201)
+            expect(interception.response.statusMessage).to.be.equal('Created')
+        })
+    });
+});
