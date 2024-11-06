1e535f0ae0932b3a21c95efa38b2f4a3ff8eb48e
Szymonke <dabrowski.szymon1999@gmail.com>
2024-11-06 17:02:36 +0100

    Added test suite for the Front Page

--- OrangeHRM/cypress/e2e/2-testing-front-page.cy.ts
+++ OrangeHRM/cypress/e2e/2-testing-front-page.cy.ts
@@ -1,3 +1,4 @@
+import { DashboardPage } from "../pages/Dashboard";
 import { Loginpage } from "../pages/Login";
 
 describe('Tests if front page works as intended', () => {
@@ -7,6 +8,9 @@ describe('Tests if front page works as intended', () => {
     });
     
     it('Tests search bar functionality', () => {
-        
+        DashboardPage.checkURL()
+        DashboardPage.checkNavigationBarContents()
+        DashboardPage.checkSearchBarFunctionality()
+        DashboardPage.checkNavigationBarFunctionality()
     });
 });
--- OrangeHRM/cypress/pages/Dashboard.ts
+++ OrangeHRM/cypress/pages/Dashboard.ts
@@ -1,8 +1,43 @@
 class Dashboard {
     private dashboardPage: string = `https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index`
 
-    private searchBar: string = `input[placeholder="Search"]`
+    private searchInput: string = `input[placeholder="Search"]`
+    private navBarItems = [
+        { page: 'Admin', url: 'https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers', selector: `//span[text()='Admin']`},
+        { page: 'PIM', url: 'https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList', selector: `//span[text()='PIM']`},
+        { page: 'Leave', url: 'https://opensource-demo.orangehrmlive.com/web/index.php/leave/viewLeaveList', selector: `//span[text()='Leave']`},
+        { page: 'Time' , url: 'https://opensource-demo.orangehrmlive.com/web/index.php/time/viewEmployeeTimesheet', selector: `//span[text()='Time']`},
+        { page: 'Recruitment', url: 'https://opensource-demo.orangehrmlive.com/web/index.php/recruitment/viewCandidates', selector: `//span[text()='Recruitment']`},
+        { page: 'My Info', url: 'https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/7', selector: `//span[text()='My Info']`},
+        { page: 'Performance', url: 'https://opensource-demo.orangehrmlive.com/web/index.php/performance/searchEvaluatePerformanceReview', selector: `//span[text()='Performance']`},
+        { page: 'Dashboard', url: 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index', selector: `//span[text()='Dashboard']`},
+        { page: 'Directory', url: 'https://opensource-demo.orangehrmlive.com/web/index.php/directory/viewDirectory', selector: `//span[text()='Directory']`},
+        { page: 'Maintenance', url: 'https://opensource-demo.orangehrmlive.com/web/index.php/maintenance/purgeEmployee', selector: `//span[text()='Maintenance']`},
+        { page: 'Claim', url: 'https://opensource-demo.orangehrmlive.com/web/index.php/claim/viewAssignClaim', selector: `//span[text()='Claim']`}]
+      
+    checkNavigationBarContents = () => {
+        this.navBarItems.forEach(item =>{
+            cy.xpath(item.selector).should('contain',item.page)
+        })
+    }
 
+    checkNavigationBarFunctionality = () => {
+        this.navBarItems.forEach(item => {
+            cy.xpath(item.selector).click()
+            cy.url().should('eql', item.url)
+            if(item.page === "Maintenance"){
+                cy.get('button.oxd-button--ghost').click()
+            }
+        })
+    }
+
+    checkSearchBarFunctionality = () => {
+        this.navBarItems.forEach(item => {
+            cy.get(this.searchInput).type(item.page)
+            cy.xpath(item.selector).should('be.visible')
+            cy.get(this.searchInput).clear()
+        })
+    }
     checkURL = () =>{
         cy.url().should('eql', this.dashboardPage)
     }
