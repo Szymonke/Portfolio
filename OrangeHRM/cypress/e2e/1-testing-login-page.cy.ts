16bb20ece2853298c51c470d0673d48433d21839
Szymonke <dabrowski.szymon1999@gmail.com>
2024-11-06 19:36:02 +0100

    Divided a login page test suite to make seperate checks for missing and invalid credentials

--- OrangeHRM/cypress/e2e/1-testing-login-page.cy.ts
+++ OrangeHRM/cypress/e2e/1-testing-login-page.cy.ts
@@ -13,11 +13,14 @@ describe('Tests if log in page works as intended', () => {
   });
 
   it('Tests invalid login credentials', () => {
-    Loginpage.submitLogin('','')
-    Loginpage.checkIfCredentialsAreFilled()
-    Loginpage.checkURL()
     Loginpage.submitLogin('wrongUsername','wrongPassword')
     Loginpage.checkIfCredentialsAreValid()
+    Loginpage.checkURL()
+  });
+
+  it('Tests missing login credentials', () => {
+    Loginpage.submitLogin('','')
+    Loginpage.checkIfCredentialsAreFilled()
     Loginpage.checkURL()
   });
 });
