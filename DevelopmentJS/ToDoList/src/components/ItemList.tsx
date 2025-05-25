534fce998cd6547f9859cf8392b96b7df77db8e2
Szymonke <dabrowski.szymon1999@gmail.com>
2025-05-25 19:50:50 +0200

    Modified current code to take advantage of react features

--- DevelopmentJS/ToDoList/src/components/ItemList.tsx
+++ DevelopmentJS/ToDoList/src/components/ItemList.tsx
@@ -1,19 +1,31 @@
-
-const addItem = () => {
-    const inputValue = document.querySelector(".newItemContent").value
-    const listElement = document.querySelector(".list")
-    const newItem = document.createElement("li")
-
-    newItem.innerHTML = inputValue
-    listElement?.appendChild(newItem)
-}
+import { useState } from "react";
 
 const ItemList = () => {
+    const [items, setItems] = useState<string[]>([])
+
+    const addItem = () => {
+        const inputElement = document.querySelector<HTMLInputElement>(".newItemContent");
+        const inputValue = inputElement ? inputElement.value : "";
+        if(inputValue) {
+            setItems([...items, inputValue])
+        }else{
+            console.log("Empty!")
+        }
+        }
     return (
         <div className="itemList">
             <input type="text" placeholder="What's the task for today?" className="newItemContent"></input>
             <div className="addButton" onClick={addItem}>Add item</div>
-            <ul className="list">
+            <ul className="list">          
+                {items.map(item => (
+                    <>
+                        <li>
+                            <div className="doneButton">Done</div>
+                            <p className="item">{item}</p>
+                            <div className="deleteButton">Delete</div>
+                        </li>
+                    </>
+                ))}
             </ul>
         </div>
     )
