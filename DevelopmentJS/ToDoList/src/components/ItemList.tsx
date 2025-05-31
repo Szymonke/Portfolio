ccb39c58de3a34b2e1a51226f72c0146a80e1fad
Szymonke <dabrowski.szymon1999@gmail.com>
2025-05-31 19:29:10 +0200

    Updated delete button, added MarkAsDone functionality, cleaned up the code

--- DevelopmentJS/ToDoList/src/components/ItemList.tsx
+++ DevelopmentJS/ToDoList/src/components/ItemList.tsx
@@ -1,35 +1,75 @@
 import { useState, type Key } from "react";
 
 const ItemList = () => {
-    const [items, setItems] = useState<{id: Key, info: string}[]>([])
+    const [items, setItems] = useState<{id: Key, info: string, theme: string, status: boolean}[]>([])
+    const [newItem, setNewItem] = useState<string>()
+    const [listTheme, setListTheme] = useState<string>("emptyList")
+
+    const getNewItem = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const targetValue = e.target.value;
+        if(targetValue) {
+            setNewItem(targetValue)
+        }else{
+            console.log("Empty!")
+        }
+    }
 
     const addItem = () => {
-        const inputElement = document.querySelector<HTMLInputElement>(".newItemContent");
-        const inputValue = inputElement ? inputElement.value : "";
-        if(inputValue) {
-            const newItem = {
+        if(newItem) {
+            setItems([...items, {
                 "id": items.length,
-                "info": inputValue
-            }
-            setItems([...items, newItem])
+                "info": newItem,
+                "theme": "incomplete",
+                "status": false
+            }])
+            if(items){setListTheme("list")}
         }else{
             console.log("Empty!")
         }
     }
     
-    const deleteItem = () => {
-        console.log("Deleted!")
+    const markAsDone = (id: Key) => {
+        items.filter((item) => {
+            if(item.id == id && item.theme == "incomplete"){
+                const index = items.indexOf(item)
+                items[index].theme = "complete"
+                items[index].status = true
+            } else if(item.id == id && item.theme == "complete"){
+                const index = items.indexOf(item)
+                items[index].theme = "incomplete"
+                items[index].status = false
+            }
+        })
+        setItems([...items])
     }
+
+    const deleteItem = (id: Key) => {
+        items.filter((item) => {
+            if(item.id == id){
+                const index = items.indexOf(item)
+                items.splice(index, 1)
+            }
+        })
+        setItems([...items])
+        if(!(items.length>0)){
+            setListTheme("emptyList")
+        }
+    }
+
     return (
         <div className="itemList">
-            <input type="text" placeholder="What's the task for today?" className="newItemContent"></input>
+            <input type="text" placeholder="What's the task for today?" className="newItemContent" onChange={getNewItem}></input>
             <div className="addButton" onClick={addItem}>Add item</div>
-            <ul className="list">          
+            <ul className={listTheme}>          
                 {items.map(item => (
-                    <li className="item" key={item.id}>
-                          <p className="itemContent">{item.info}</p>
-                         <div className="deleteButton" onClick={deleteItem}>Done</div>
-                     </li>
+                    <li className="item" key={item.id} data-key={item.id}>
+                        <p className={item.theme} onClick={() => {
+                            markAsDone(item.id)
+                        }}>{item.info}</p>
+                        <img src="src\assets\images\recycle-bin.png"className="deleteButton" onClick={() => {
+                            deleteItem(item.id)
+                        }}></img>
+                    </li>
                 ))}
             </ul>
         </div>
